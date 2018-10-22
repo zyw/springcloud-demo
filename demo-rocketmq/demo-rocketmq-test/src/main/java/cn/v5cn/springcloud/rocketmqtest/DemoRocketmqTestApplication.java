4a2c4be4882d0eedab38c122334e29a76b787bb7
zyw090111@163.com <zyw090111@163.com>
2018-10-22 18:38:35 +0800

    RocketMQ

---
+++ demo-rocketmq/demo-rocketmq-test/src/main/java/cn/v5cn/springcloud/rocketmqtest/DemoRocketmqTestApplication.java
@@ -0,0 +1,12 @@
+package cn.v5cn.springcloud.rocketmqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRocketmqTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRocketmqTestApplication.class, args);
+    }
+}
