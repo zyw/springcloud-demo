b017dee19d7883389729eb50fb5de3e94b265d55
zyw090111@163.com <zyw090111@163.com>
2018-10-22 18:31:50 +0800

    RocketMQ

---
+++ demo-rocketmq/demo-rocketmq-test/src/main/java/cn/v5cn/springcloud/rocketmqtest/demo/Consumer.java
@@ -0,0 +1,30 @@
+package cn.v5cn.springcloud.rocketmqtest.demo;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class Consumer {
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("nsop-test");
+        consumer.setNamesrvAddr("154.8.143.230:9876");
+        consumer.subscribe("TopicTest","*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n",Thread.currentThread().getName(),msgs);
+
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.start();
+
+        System.out.printf("Consumer Started.%n");
+    }
+}
---
+++ demo-rocketmq/demo-rocketmq-test/src/main/java/cn/v5cn/springcloud/rocketmqtest/demo/SyncProducer.java
@@ -0,0 +1,33 @@
+package cn.v5cn.springcloud.rocketmqtest.demo;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+
+public class SyncProducer {
+
+    public static void main(String[] args) {
+        try {
+            DefaultMQProducer producer = new DefaultMQProducer("nsop-test");
+            producer.setNamesrvAddr("154.8.143.230:9876");
+            producer.start();
+            for (int i = 0; i < 5; i++) {
+                Message message = new Message("TopicTest"/*Topic*/,
+                        "TagA"/*Tag*/,
+                        ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET)/*Message body*/);
+                SendResult sendResult = producer.send(message);
+                System.out.printf("%s%n",sendResult);
+            }
+
+            producer.shutdown();
+        } catch (MQClientException | UnsupportedEncodingException | RemotingException | MQBrokerException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
